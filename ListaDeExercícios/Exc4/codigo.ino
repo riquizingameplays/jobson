e8aca99dc10bc5e47e737f44674f4d9788de7eae
riquizingameplays <99329616+riquizingameplays@users.noreply.github.com>
2022-03-16 07:50:21 -0300

    Create codigo.ino

---
+++ ListaDeExercícios/Exc4/codigo.ino
@@ -0,0 +1,24 @@
+const int led1 = 7;
+const int bot1 = 4;
+
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(bot1, INPUT);
+}
+
+int var1;
+  
+void loop()
+{
+  var1 = digitalRead(bot1);
+  
+  if(var1){
+   digitalWrite(led1, HIGH);
+    
+  }else{
+   digitalWrite(led1, LOW);
+    
+  }
+  delay(10);
+}
